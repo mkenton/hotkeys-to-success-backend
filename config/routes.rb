1b244cb65d3504c8d41d1dd23d6ef4b0b86e2d1f
Michael <mkenton2020@icloud.com>
2021-10-26 16:34:53 -0500

    lessoncomplete route to create

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post '/auto_login', to: 'auth#auto_login'
   get '/profile', to: 'users#profile'
   get '/logged_in', to: 'application#logged_in?'
-  post '/lessoncomplete', to: 'lesson_events#update'
+  post '/lessoncomplete', to: 'lesson_events#create'
 
 
 end
